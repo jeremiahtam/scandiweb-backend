7dd08b6af90c231549e1a9e5a2b64e90ce21bd02
Esite Jeremiah Tamarankuroemi <jerry4destinychild@yahoo.com>
2022-08-14 13:55:26 +0100

    update

--- config/config.php
+++ config/config.php
@@ -1,18 +1,18 @@
 <?php
 // DB Params
 define('DB_HOST', 'localhost');
-// define('DB_USER', 'id18999226_scandiweb_tam');
-// define('DB_PASS', 'd*0Ve721x+vvYy&<');
-// define('DB_NAME', 'id18999226_scandiweb');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'scandiweb');
+define('DB_USER', 'id18999226_scandiweb_tam');
+define('DB_PASS', 'd*0Ve721x+vvYy&<');
+define('DB_NAME', 'id18999226_scandiweb');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'scandiweb');
 
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
-define('URLROOT', 'http://localhost/scandiweb-backend');
-// define('URLROOT', 'https://scandiweb-junior-web-dev.000webhostapp.com/');
+// define('URLROOT', 'http://localhost/scandiweb-backend');
+define('URLROOT', 'https://scandiweb-junior-web-dev.000webhostapp.com/');
 // Site Name
 define('SITENAME', 'Scandiweb Test Website');
 // App Version
